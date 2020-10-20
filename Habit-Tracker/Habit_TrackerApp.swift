b4e1d9e67e124d0628fc093a0a6a9bbd96d1ee54
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-10-20 11:56:27 +0530

    first commit

---
+++ Habit-Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit-Tracker
+//
+//  Created by PRABALJIT WALIA     on 20/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
