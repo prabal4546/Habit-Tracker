1c365c303734842aecadca9e2158738e1ddc3a13
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-10-20 14:42:47 +0530

    second commit

--- Habit-Tracker/ContentView.swift
+++ Habit-Tracker/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+struct Activity:Identifiable, Codable{
+    let id = UUID()
+    let activityTitle:String
+    let activityDescription:String
+}
+class holdActivities:ObservableObject{
+    @Published var activities = [Activity](){
+        didSet{
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(activities){
+                UserDefaults.standard.set(encoded, forKey: "Activities")
+            }
+        }
+            
+    }
+        init(){
+            if let items = UserDefaults.standard.data(forKey: "Activities"){
+                let decoder = JSONDecoder()
+                if let decoded = try? decoder.decode([Activity].self, from: items){
+                    self.activities = decoded
+                    return
+                }
+            }
+            self.activities = []
+        }
+    }
+
 struct ContentView: View {
+    
+    @State private var activity = ""
+    @State private var description = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        NavigationView{
+            Form{
+                TextField("Activity", text: $activity)
+                    .font(.headline)
+                TextField("Description",text:$description)
+            }
+            .navigationTitle("My Habits")
+        }
     }
 }
 
